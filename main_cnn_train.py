1e04383ecb24c9f1077db1e3f8fa59b0b7609b24
Francesco Nuzzo <franuzzo966@gmail.com>
2021-03-27 13:10:33 +0100

    Add progress bar, batch and fix model saving

--- main_cnn_train.py
+++ main_cnn_train.py
@@ -1,9 +1,11 @@
 import os
 from datetime import datetime
+import pathlib
 
 import torch.optim as optim
 import torch.nn as nn
 import torch
+from tqdm import tqdm
 
 from tests.cnn_dataloader import get_loaders_cifar10
 from tests.cnn_model import LeNet5
@@ -16,17 +18,18 @@ if __name__ == "__main__":
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print(f'Using device {device}')    
 
-    trainloader, testloader, classes = get_loaders_cifar10()
+    trainloader, testloader, classes = get_loaders_cifar10(batch_size=16)
 
     model = LeNet5()
     model.to(device)
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-
+    
     for epoch in range(2):  # loop over the dataset multiple times
         running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
+        pbar = tqdm(trainloader, 0)
+        for i, data in enumerate(pbar):
             # get the inputs; data is a list of [inputs, labels]
             # inputs, labels = data
             inputs, labels = data[0].to(device), data[1].to(device)
@@ -41,15 +44,13 @@ if __name__ == "__main__":
             optimizer.step()
 
             # print statistics
-            running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' %
-                    (epoch + 1, i + 1, running_loss / 2000))
-                running_loss = 0.0
+            pbar.set_description(f'Training loss: {loss.item()}')
+
 
     if SAVE_MODEL:
         now_time = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-        filename = f'{model.name}_{now_time}'
+        filename = f'{model.name}_{now_time}.pth'
         save_path = os.path.join(MODELS_FOLDER, filename)
+        os.makedirs(MODELS_FOLDER, exist_ok=True)
         torch.save(model.state_dict(), save_path)
     print('Finished Training')
