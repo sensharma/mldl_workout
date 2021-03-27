e640933456f9e9eb76d7cf7f711ec8e034a9f27f
Francesco Nuzzo <franuzzo966@gmail.com>
2021-03-27 13:09:39 +0100

    Add batch_size option in dataloader

--- tests/cnn_dataloader.py
+++ tests/cnn_dataloader.py
@@ -3,7 +3,7 @@ import torchvision
 import torchvision.transforms as transforms
 
 
-def get_loaders_cifar10():
+def get_loaders_cifar10(batch_size=4):
 
     transform = transforms.Compose(
         [transforms.ToTensor(),
@@ -11,12 +11,12 @@ def get_loaders_cifar10():
 
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                             download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                             shuffle=True, num_workers=2)
 
     testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                         download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                             shuffle=False, num_workers=2)
 
     classes = trainset.classes
